343afd159c0734230390412049bbdd02fb2f3cb3
João Marcos <joaomarcos.vidcar@gmail.com>
2022-02-27 18:15:51 -0300

    Condições em JavaScript

---
+++ Exercicícios/Aula11/ex008.js
@@ -0,0 +1,6 @@
+var velocidade = 78.2
+console.log(`A velocidade do seu carro é ${velocidade}Km/h`)
+if(velocidade > 60) { // Condição Simples
+    console.log('Você ultrapassou a velocidade permitida. MULTADO!')
+}
+console.log('Dirija sempre usando o cinto de segurança.')
